05c99bf0aa1f20c22fed8301ba118fd61d414966
Michael Mortensen <michael@geekle.io>
2024-09-15 23:14:00 +0200

    :pencil2: typo

--- test/Codebelt.Extensions.Xunit.Hosting.AspNetCore.Tests/Assets/BoolMiddleware.cs
+++ test/Codebelt.Extensions.Xunit.Hosting.AspNetCore.Tests/Assets/BoolMiddleware.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Cuemon.AspNetCore;
-using Cuemon.AspNetCore.Http.Headers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 
@@ -10,25 +9,25 @@ namespace Codebelt.Extensions.Xunit.Hosting.AspNetCore.Assets
     public class BoolMiddleware : ConfigurableMiddleware<BoolOptions>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="CorrelationIdentifierMiddleware"/> class.
+        /// Initializes a new instance of the <see cref="BoolMiddleware"/> class.
         /// </summary>
         /// <param name="next">The delegate of the request pipeline to invoke.</param>
-        /// <param name="setup">The <see cref="CorrelationIdentifierOptions" /> which need to be configured.</param>
+        /// <param name="setup">The <see cref="BoolMiddleware" /> which need to be configured.</param>
         public BoolMiddleware(RequestDelegate next, IOptions<BoolOptions> setup) : base(next, setup)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CorrelationIdentifierMiddleware"/> class.
+        /// Initializes a new instance of the <see cref="BoolMiddleware"/> class.
         /// </summary>
         /// <param name="next">The delegate of the request pipeline to invoke.</param>
-        /// <param name="setup">The <see cref="CorrelationIdentifierOptions"/> which need to be configured.</param>
+        /// <param name="setup">The <see cref="BoolMiddleware"/> which need to be configured.</param>
         public BoolMiddleware(RequestDelegate next, Action<BoolOptions> setup) : base(next, setup)
         {
         }
 
         /// <summary>
-        /// Executes the <see cref="CorrelationIdentifierMiddleware" />.
+        /// Executes the <see cref="BoolMiddleware" />.
         /// </summary>
         /// <param name="context">The context of the current request.</param>
         /// <returns>A task that represents the execution of this middleware.</returns>
