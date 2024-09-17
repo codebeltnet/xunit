0c04adbd3cf98088d03c38cdfd8935e715cb3ab4
Michael Mortensen <michael@geekle.io>
2024-09-17 20:49:01 +0200

    :white_check_mark: added complementary unit tests

---
+++ test/Codebelt.Extensions.Xunit.Hosting.AspNetCore.Tests/MvcAspNetCoreHostTestTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Codebelt.Extensions.Xunit.Hosting.AspNetCore.Assets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Codebelt.Extensions.Xunit.Hosting.AspNetCore
+{
+    public class MvcAspNetCoreHostTestTest : AspNetCoreHostTest<AspNetCoreHostFixture>
+    {
+        private readonly AspNetCoreHostFixture _hostFixture;
+        private readonly HttpClient _client;
+
+        public MvcAspNetCoreHostTestTest(AspNetCoreHostFixture hostFixture, ITestOutputHelper output = null, Type callerType = null) : base(hostFixture, output, callerType)
+        {
+            hostFixture.ServiceProvider.GetRequiredService<ITestOutputHelperAccessor>().TestOutput = output;
+            _hostFixture = hostFixture;
+            _client = hostFixture.Host.GetTestClient();
+        }
+
+        [Fact]
+        public async Task GetTestAsync()
+        {
+            var response = await _client.GetAsync("/Fake");
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Assert.Equal("Unit Test", body);
+        }
+
+        [Fact]
+        public async Task GetTestAsync2()
+        {
+            var response = await _client.GetAsync("/Fake");
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Assert.Equal("Unit Test", body);
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddApplicationPart(typeof(FakeController).Assembly);
+
+            services.AddXunitTestLoggingOutputHelperAccessor();
+            services.AddXunitTestLogging(new TestOutputHelperAccessor(TestOutput));
+        }
+
+        public override void ConfigureApplication(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(routes => { routes.MapControllers(); });
+        }
+    }
+}
