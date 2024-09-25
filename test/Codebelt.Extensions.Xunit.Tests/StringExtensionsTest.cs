457d9f20fae13c8a8bcbdebb67a1dedba535652c
Michael Mortensen <michael@geekle.io>
2024-09-25 23:49:03 +0200

    :twisted_rightwards_arrows: adapt ReplaceLineEndings for netstandard2.0 (moved from Cuemon.Extensions)

---
+++ test/Codebelt.Extensions.Xunit.Tests/StringExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Codebelt.Extensions.Xunit
+{
+    public class StringExtensionsTest : Test
+    {
+        public StringExtensionsTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void ReplaceLineEndings_ShouldReplaceNewLineOccurrences()
+        {
+            var lineEndings = "Windows has \r\n (CRLF) and Linux has \n (LF)";
+
+            TestOutput.WriteLine($$"""
+                                   Before: {{lineEndings}}
+                                   After: {{lineEndings.ReplaceLineEndings()}}
+                                   """);
+
+            TestOutput.WriteLine(RuntimeInformation.OSDescription);
+            TestOutput.WriteLine(RuntimeInformation.FrameworkDescription);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Assert.Equal("Windows has \n (CRLF) and Linux has \n (LF)", lineEndings.ReplaceLineEndings());
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Assert.Equal("Windows has \r\n (CRLF) and Linux has \r\n (LF)", lineEndings.ReplaceLineEndings());
+            }
+        }
+    }
+}
