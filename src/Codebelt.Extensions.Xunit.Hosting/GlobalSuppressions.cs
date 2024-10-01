15259d6ec1fa777008e6aff6cdfd94b3b085251d
Michael Mortensen <michael@geekle.io>
2024-10-01 23:17:34 +0200

    V9.0.0/private assets experiment (#12)
    :alembic: experiment with PrivateAssets set to compile
    
    :twisted_rightwards_arrows: further reduce dependency to Cuemon
    
    :construction_worker: updated CI/CD

---
+++ src/Codebelt.Extensions.Xunit.Hosting/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S3881:\"IDisposable\" should be implemented correctly", Justification = "This is an implementation of the IDisposable interface tailored to avoid wrong implementations.", Scope = "type", Target = "~T:Codebelt.Extensions.Xunit.Hosting.HostFixture")]
