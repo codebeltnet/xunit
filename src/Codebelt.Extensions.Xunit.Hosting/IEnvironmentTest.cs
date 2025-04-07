7c866957b8c1235b32def611620dcac8fe92b3dd
Michael Mortensen <michael@geekle.io>
2025-04-07 16:50:36 +0200

    :truck: rename to IEnvironmentTest

--- src/Codebelt.Extensions.Xunit.Hosting/IEnvironmentTest.cs
+++ src/Codebelt.Extensions.Xunit.Hosting/IEnvironmentTest.cs
@@ -3,14 +3,14 @@
 namespace Codebelt.Extensions.Xunit.Hosting
 {
     /// <summary>
-    /// Represents the members needed for DI testing with support for HostingEnvironment.
+    /// Represents the members needed for DI testing with support for <see cref="IHostEnvironment"/>.
     /// </summary>
-    public interface IHostingEnvironmentTest
+    public interface IEnvironmentTest
     {
         /// <summary>
         /// Gets the <see cref="IHostEnvironment"/> initialized by the <see cref="IHost"/>.
         /// </summary>
         /// <value>The <see cref="IHostEnvironment"/> initialized by the <see cref="IHost"/>.</value>
-        IHostEnvironment HostingEnvironment { get; }
+        IHostEnvironment Environment { get; }
     }
 }
