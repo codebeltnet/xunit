264e094e960bba3bbab84588698e0e8fd1de0e72
Michael Mortensen <michael@geekle.io>
2025-04-09 21:02:03 +0200

    :white_check_mark: complemental test for xunit test logger

--- test/Codebelt.Extensions.Xunit.Hosting.Tests/ServiceCollectionExtensions.cs
+++ test/Codebelt.Extensions.Xunit.Hosting.Tests/ServiceCollectionExtensions.cs
@@ -18,6 +18,61 @@ namespace Codebelt.Extensions.Xunit.Hosting
         public void AddXunitTestLogging_ShouldAddXunitTestLogging()
         {
             var services = new ServiceCollection();
+            services.AddXunitTestLogging();
+
+            var provider = services.BuildServiceProvider();
+
+            var logger1 = provider.GetRequiredService<ILogger<ServiceCollectionExtensions>>();
+            var loggerStore1 = logger1.GetTestStore();
+            var loggerStore1Duplicate = logger1.GetTestStore(typeof(ServiceCollectionExtensions).FullName!.ToLowerInvariant());
+
+            var logger2 = provider.GetRequiredService<ILogger<Test>>();
+            var loggerStore2 = logger2.GetTestStore(null); // all loggers
+
+            logger1.LogCritical("SUT");
+            logger1.LogTrace("SUT");
+            logger1.LogDebug("SUT");
+            logger1.LogError("SUT");
+            logger1.LogInformation("SUT");
+            logger1.LogWarning("SUT");
+
+            logger2.LogInformation("Unique message for logger2.");
+
+            Assert.Throws<KeyNotFoundException>(() => logger2.GetTestStore("InvalidValue"));
+
+            Assert.Equal(loggerStore1, loggerStore1Duplicate);
+
+            Assert.NotNull(logger1);
+            Assert.NotNull(loggerStore1);
+            
+            Assert.NotNull(logger2);
+            Assert.NotNull(loggerStore2);
+
+            Assert.Equal(6, loggerStore1.Query().Count());
+            Assert.Equal(7, loggerStore2.Query().Count());
+            
+            Assert.Collection(loggerStore1.Query(),
+                entry => Assert.Equal("Critical: SUT", entry.ToString()),
+                entry => Assert.Equal("Trace: SUT", entry.ToString()),
+                entry => Assert.Equal("Debug: SUT", entry.ToString()),
+                entry => Assert.Equal("Error: SUT", entry.ToString()),
+                entry => Assert.Equal("Information: SUT", entry.ToString()),
+                entry => Assert.Equal("Warning: SUT", entry.ToString()));
+
+            Assert.Collection(loggerStore2.Query(),
+                entry => Assert.Equal("Critical: SUT", entry.ToString()),
+                entry => Assert.Equal("Trace: SUT", entry.ToString()),
+                entry => Assert.Equal("Debug: SUT", entry.ToString()),
+                entry => Assert.Equal("Error: SUT", entry.ToString()),
+                entry => Assert.Equal("Information: SUT", entry.ToString()),
+                entry => Assert.Equal("Warning: SUT", entry.ToString()),
+                entry => Assert.Equal("Information: Unique message for logger2.", entry.ToString()));
+        }
+
+        [Fact]
+        public void AddXunitTestLogging_ShouldAddXunitTestLoggingWithTestOutput()
+        {
+            var services = new ServiceCollection();
             services.AddXunitTestLogging(TestOutput);
 
             var provider = services.BuildServiceProvider();
@@ -67,7 +122,6 @@ namespace Codebelt.Extensions.Xunit.Hosting
                 entry => Assert.Equal("Information: SUT", entry.ToString()),
                 entry => Assert.Equal("Warning: SUT", entry.ToString()),
                 entry => Assert.Equal("Information: Unique message for logger2.", entry.ToString()));
-
         }
     }
 }
