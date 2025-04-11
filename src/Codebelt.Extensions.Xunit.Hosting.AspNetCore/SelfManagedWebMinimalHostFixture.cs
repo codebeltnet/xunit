03d5adaec0bebabd0f3f06b17fa0be8b0f1ade87
Michael Mortensen <michael@geekle.io>
2025-04-12 00:30:12 +0200

    :sparkles: added self-managed variants of managed web host fixtures

---
+++ src/Codebelt.Extensions.Xunit.Hosting.AspNetCore/SelfManagedWebMinimalHostFixture.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Codebelt.Extensions.Xunit.Hosting.AspNetCore
+{
+    /// <summary>
+    /// Represents a self-managed implementation of the <see cref="ManagedMinimalHostFixture"/> class.
+    /// </summary>
+    public sealed class SelfManagedWebMinimalHostFixture : ManagedWebMinimalHostFixture
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelfManagedWebMinimalHostFixture"/> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor sets the <see cref="HostFixture.AsyncHostRunnerCallback"/> to a no-op asynchronous delegate.
+        /// </remarks>
+        public SelfManagedWebMinimalHostFixture()
+        {
+            AsyncHostRunnerCallback = (_, __) => Task.CompletedTask;
+        }
+    }
+}
